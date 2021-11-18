4a6bbdd579d9ebafe58c0d8ba42e7409de0f8946
planet36 <planet36@users.noreply.github.com>
2021-11-17 22:39:50 -0500

    Add download-python.bash

---
+++ download-python.bash
@@ -0,0 +1,24 @@
+
+# scripts
+for F in \
+Nd-arange
+do
+	if [[ ! -f "$F" ]]
+	then
+		echo "$F"
+		curl -O https://raw.githubusercontent.com/planet36/dotfiles/main/link/.local/bin/"$F"
+	fi
+done
+
+# modules
+for F in \
+arange.py \
+grouper.py \
+remove_exponent.py
+do
+	if [[ ! -f "$F" ]]
+	then
+		echo "$F"
+		curl -O https://raw.githubusercontent.com/planet36/dotfiles/main/link/.local/lib/python/"$F"
+	fi
+done
