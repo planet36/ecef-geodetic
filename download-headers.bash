77a314b2a84936965ffe9c7eb15dc6189434b69e
planet36 <planet36@users.noreply.github.com>
2021-11-17 22:38:59 -0500

    Move URL to where it's used

--- download-headers.bash
+++ download-headers.bash
@@ -1,6 +1,5 @@
 
-ROOT_URL='https://raw.githubusercontent.com/planet36/dotfiles/main/link/.local/include'
-
+# header files
 for F in \
 angle-utils.hpp \
 angle.hpp \
@@ -14,6 +13,6 @@ do
 	if [[ ! -f "$F" ]]
 	then
 		echo "$F"
-		curl -O "$ROOT_URL/$F"
+		curl -O https://raw.githubusercontent.com/planet36/dotfiles/main/link/.local/include/"$F"
 	fi
 done
