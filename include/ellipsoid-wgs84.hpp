79b4869cde7626acacbd0f56c3d47573fdae5f12
Steven Ward <planet36@users.noreply.github.com>
2026-02-21 08:34:46 -0500

    Change license from OSL-3.0 to MPL-2.0

--- include/ellipsoid-wgs84.hpp
+++ include/ellipsoid-wgs84.hpp
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: Steven Ward
-// SPDX-License-Identifier: OSL-3.0
+// SPDX-License-Identifier: MPL-2.0
 
 /// WGS 84 Reference Ellipsoid
 /**
--- include/isclose.hpp
+++ include/isclose.hpp
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: Steven Ward
-// SPDX-License-Identifier: OSL-3.0
+// SPDX-License-Identifier: MPL-2.0
 
 /// function to determine if two floating-point numbers are close to each other
 /**
